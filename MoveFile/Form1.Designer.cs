1dee5f8d2f14149e4a781e81f9c5323d65c85f46
Administrator <cl456852@gmail.com>
2021-12-13 16:41:35 +0800

    DDDD

--- MoveFile/Form1.Designer.cs
+++ MoveFile/Form1.Designer.cs
@@ -32,30 +32,32 @@
             this.Insert = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dataGridView1.Location = new System.Drawing.Point(-3, 84);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(1244, 32);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1732, 669);
+            this.dataGridView1.Size = new System.Drawing.Size(1061, 1366);
             this.dataGridView1.TabIndex = 1;
             // 
             // Insert
             // 
             this.Insert.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.Insert.Location = new System.Drawing.Point(1106, 18);
-            this.Insert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Insert.Location = new System.Drawing.Point(1475, 330);
+            this.Insert.Margin = new System.Windows.Forms.Padding(5);
             this.Insert.Name = "Insert";
-            this.Insert.Size = new System.Drawing.Size(92, 34);
+            this.Insert.Size = new System.Drawing.Size(123, 45);
             this.Insert.TabIndex = 2;
             this.Insert.Text = "Insert";
             this.Insert.UseVisualStyleBackColor = true;
@@ -64,36 +66,60 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1310, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(1747, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.Size = new System.Drawing.Size(82, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(943, 24);
+            this.button1.Location = new System.Drawing.Point(1257, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 28);
+            this.button1.Size = new System.Drawing.Size(117, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "MovePic";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(-4, 62);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 37;
+            this.dataGridView2.Size = new System.Drawing.Size(1192, 1300);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1389, 676);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 63);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "find";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1726, 838);
+            this.ClientSize = new System.Drawing.Size(2301, 1423);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Insert);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,7 +131,8 @@
         private System.Windows.Forms.Button Insert;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- MoveFile/Form1.cs
+++ MoveFile/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.Collections;
 using System.IO;
 using BencodeLibrary;
+using System.Text.RegularExpressions;
 
 namespace MoveFile
 {
@@ -17,31 +18,64 @@ namespace MoveFile
         public Form1()
         {
             InitializeComponent();
+            list = new ArrayList();
+            tdDownloadList = new ArrayList();
+
+            list.Add("D:\\TDDOWNLOAD20160214");
+            list.Add("E:\\tdDownloadY");
+            list.Add("E:\\TDDOWNLOADE");
+            list.Add("E:\\ThunderDownloadRemote");
+            list.Add("E:\\ThunderDownloadRemoteA");
+            list.Add("E:\\tdDownloadA");
+            list.Add("E:\\TDDOWNLOADADMIN");
+            list.Add("E:\\迅雷下载");
+            list.Add("E:\\tdDownloadz");
+            list.Add("D:\\迅雷下载");
+            list.Add("Z:\\tddownloada");
+            list.Add("Z:\\tddownloadb");
+            list.Add("Z:\\tddownloadc");
+            list.Add("Z:\\迅雷下载");
+            list.Add("E:\\tdDownloadAdmin");
+            list.Add("E:\\dlA");
+            list.Add("Z:\\tdDownloadd1");
+            //list.Add("E:\\utorrentDownload");
+            //list.Add("E:\\utorrentRemote");
+            tdDownloadList.AddRange(list);
+            tdDownloadList.Add("e:\\abcd");
+            tdDownloadList.Add("d:\\abcd");
+            tdDownloadList.Add("E:\\新建文件夹 (2)");
+            tdDownloadList.Add("I:\\98");
+            tdDownloadList.Add("J:\\99");
+            tdDownloadList.Add("Z:\\abcd");
+            list.Add("Z:\\tdDownloadd1");
+
         }
 
         ArrayList list;
         ArrayList moveList;
         ArrayList moveList1;
+        ArrayList singleFileList;
+        ArrayList tdDownloadList;
 
         void init()
         {
             moveList1 = new ArrayList();
             moveList = new ArrayList();
-            list = new ArrayList();
-        }
         
+            singleFileList = new ArrayList();
+        }
+
         private void Insert_Click(object sender, EventArgs e)
         {
             init();
             totalLength = 0;
-            list.Add("D:\\THUNDER20150108");
-            list.Add("D:\\QQDownload");
-      
-            //list.Add("D:\\VuzeDownloads1");
-            //list.Add("D:\\新建文件夹");
+
+            processUtorrent();
             process();
             moveFile();
-            //test();
+
+
+
         }
 
         void test()
@@ -53,7 +87,7 @@ namespace MoveFile
         double totalLength = 0;
         private void process()
         {
-            
+
             foreach (string s in list)
             {
                 process(s);
@@ -63,62 +97,130 @@ namespace MoveFile
             dataGridView1.Refresh();
         }
 
-        private void process(string folderPath)
+        private void processUtorrent()
         {
-             
-            
-            DirectoryInfo TheFolder = new DirectoryInfo(folderPath);
-            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories("*",SearchOption.TopDirectoryOnly))
+            DirectoryInfo TheFolder = new DirectoryInfo("D:\\VuzeMove");
+            if (!TheFolder.Exists)
+                return;
+            bool hasIncomplete = false;
+            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories("*", SearchOption.TopDirectoryOnly))
             {
-                int torrentNumber = 0;
-                bool hasIncomplete=false;
-                bool hasBigFile=false;
+                hasIncomplete = false;
                 FileInfo[] fileInfos = NextFolder.GetFiles("*", SearchOption.AllDirectories);
-                double folderLength = 0;
-                foreach(FileInfo f in fileInfos)
+                foreach (FileInfo f in fileInfos)
                 {
-
-                    if (f.Length / 1024 / 1024 > 60 && (f.Extension == ".bt!" || f.Extension == ".!ut" || f.Extension == ".bc!" || f.Extension == ".az!" || f.Extension == ".td" || f.Extension == ".tdl"))
+                    if(f.Extension == ".bt!" || f.Extension == ".!ut" || f.Extension == ".bc!" || f.Extension == ".az!" || f.Extension == ".td" || f.Extension == ".tdl"||f.Extension== ".xltd")
                     {
                         hasIncomplete = true;
-                        continue;
+                        break;
                     }
-                    else if (f.Length / 1024 / 1024 > 60)
-                    {
-                        hasBigFile = true;
-                    }
-                    if (f.Extension.ToLower() == ".torrent")
-                    {
-                        torrentNumber++;
-                    }
-                    folderLength += f.Length/1024/1024;
                 }
-                if (!hasIncomplete && hasBigFile)
+                if(!hasIncomplete)
                 {
-                    if (torrentNumber > 1)
+                    moveList.Add(new MyFileInfo(null, NextFolder.FullName));
+                }
+            }
+        }
+
+        private void process(string folderPath)
+        {
+
+
+            DirectoryInfo TheFolder = new DirectoryInfo(folderPath);
+            if (TheFolder.Exists)
+            {
+                foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories("*", SearchOption.TopDirectoryOnly))
+                {
+                    try
                     {
-                        moveList1.Add(new MyFileInfo(NextFolder.FullName));
+                        int torrentNumber = 0;
+                        bool hasIncomplete = false;
+                        bool hasBigFile = false;
+                        FileInfo[] fileInfos = NextFolder.GetFiles("*", SearchOption.AllDirectories);
+                        double folderLength = 0;
+                        foreach (FileInfo f in fileInfos)
+                        {
+
+                            if (f.Length / 1024 / 1024 > 60 && (f.Extension == ".bt!" || f.Extension == ".!ut" || f.Extension == ".bc!" || f.Extension == ".az!" || f.Extension == ".td" || f.Extension == ".tdl"|| f.Extension == ".xltd"))
+                            {
+                                hasIncomplete = true;
+                                continue;
+                            }
+                            else if (f.Length / 1024 / 1024 > 35)
+                            {
+                                hasBigFile = true;
+                            }
+                            if (f.Extension.ToLower() == ".torrent")
+                            {
+                                torrentNumber++;
+                            }
+                            folderLength += f.Length / 1024 / 1024;
+                        }
+                        if (!hasIncomplete && hasBigFile)
+                        {
+                            if (torrentNumber > 1)
+                            {
+                                moveList1.Add(new MyFileInfo(null, NextFolder.FullName));
+                            }
+                            else
+                                moveList.Add(new MyFileInfo(null, NextFolder.FullName));
+                            totalLength += folderLength;
+
+                        }
+
                     }
-                    else
-                        moveList.Add(new MyFileInfo ( NextFolder.FullName));
-                    totalLength += folderLength;
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
 
                 }
-              
+                foreach (FileInfo f in TheFolder.GetFiles("*", SearchOption.TopDirectoryOnly))
+                {
+                    if (f.Length / 1024 / 1024 > 60 && (f.Extension != ".bt!" && f.Extension != ".!ut" && f.Extension != ".bc!" && f.Extension != ".az!" && f.Extension != ".td" && f.Extension != ".tdl"&& f.Extension != ".xltd"))
+                    {
 
-
+                        singleFileList.Add(f.FullName);
+                    }
+                }
             }
-           
+
+             
          
+
         }
 
         private void moveFile()
         {
+            foreach (string path in singleFileList)
+            {
+                string newPath = path[0] + ":\\abcd\\finish\\" + Path.GetFileName(path);
+                try
+                {
+                    File.Move(path, newPath);
+                }
+                catch (Exception e)
+                {
+                    if (e.Message.Contains("已存在"))
+                    {
+                        Random rand = new Random();//生成随机数  
+                        string prefix = "";
+
+                        prefix += rand.Next();
+                        string fileName = Path.GetFileName(newPath);
+                        fileName = rand.Next() + "_" + fileName;
+                        newPath = path[0] + ":\\abcd\\finish\\" + fileName;
+                        File.Move(path, newPath);
+                    }
+                    else
+                        MessageBox.Show("old:" + path + "       new:" + newPath + "     " + e.Message);
+                }
+            }
             foreach (MyFileInfo myfileInfo in moveList)
             {
-                string newDir="";
-                bool hasTorrent=false;
-                string torrentName="";
+                string newDir = "";
+                bool hasTorrent = false;
+                string torrentName = "";
                 string folder = myfileInfo.Path;
                 DirectoryInfo TheFolder = new DirectoryInfo(folder);
                 foreach (FileInfo file in TheFolder.GetFiles())
@@ -129,19 +231,31 @@ namespace MoveFile
                         torrentName = getName(file.FullName);
                     }
                 }
-                if(hasTorrent)
+                if (hasTorrent)
                     newDir = folder[0] + ":\\abcd\\finish\\" + torrentName;
                 else
                     newDir = folder[0] + ":\\abcd\\finish\\" + new DirectoryInfo(folder).Name;
                 try
                 {
-               
+
                     Directory.Move(folder, newDir);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
+                    if (e.Message.Contains("已存在"))
+                    {
+                        string dirName = new DirectoryInfo(newDir).Name;
+                        Random rand = new Random();//生成随机数  
+                        string prefix = "";
 
-                    MessageBox.Show("old:"+folder+"       new:"+newDir +"     "+e.Message);
+                        prefix += rand.Next();
+
+                        dirName = prefix + "_" + dirName;
+
+                        dirName = Path.Combine(folder[0] + ":\\abcd\\finish\\", dirName);
+                        Directory.Move(folder, dirName);
+                    } else
+                        MessageBox.Show("old:" + folder + "       new:" + newDir + "     " + e.Message);
                 }
 
             }
@@ -175,31 +289,180 @@ namespace MoveFile
             init();
             DirectoryInfo TheFolder = new DirectoryInfo("d:\\utorrentFinish");
             bool isPicFolder;
-            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories("*",SearchOption.TopDirectoryOnly))
+
+            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories("*", SearchOption.TopDirectoryOnly))
             {
-                isPicFolder = true;
-                FileInfo[] fileInfos = NextFolder.GetFiles("*", SearchOption.AllDirectories);
-                foreach(FileInfo fileInfo in fileInfos)
+                try
                 {
-                    if (fileInfo.Length / 1024 / 2024 > 25)
+                    isPicFolder = true;
+                    FileInfo[] fileInfos = NextFolder.GetFiles("*", SearchOption.AllDirectories);
+                    foreach (FileInfo fileInfo in fileInfos)
                     {
-                        isPicFolder = false;
-                        break;
+                        if (fileInfo.Length / 1024 / 2024 > 25)
+                        {
+                            isPicFolder = false;
+                            break;
+                        }
+
                     }
-             
+                    if (isPicFolder)
+                    {
+                        moveList.Add(NextFolder);
+                    }
+
                 }
-                if(isPicFolder)
+                catch (Exception ex)
                 {
-                    moveList.Add(NextFolder);
-                 }
-           
+                    Console.WriteLine(ex.Message);
+                }
+
+
             }
             foreach (DirectoryInfo info in moveList)
             {
                 Directory.Move(info.FullName, Path.Combine("d:\\abcd\\pic", info.ToString()).ToString());
             }
+
+
             dataGridView1.DataSource = moveList;
             dataGridView1.Refresh();
+        }
+
+        Dictionary<string, ArrayList> dic = new Dictionary<string, ArrayList>();
+        Regex idRegex1 = new Regex("[A-Z]{1,}-[0-9]{1,}|[A-Z]{1,}[0-9]{1,}|[A-Z]{1,}‐[0-9]{1,}");
+        ArrayList resultList = new ArrayList();
+        ArrayList arrayList = new ArrayList() { "-1.", "-2.", "-3.", "-4.", "-5.", "-6.", "-7.", "-8.", "A.", "C.", "D.", "E.", "F.", "G.", "H.", "I.", "-A-", "-B-", "-C-", "-D-", "-E-", "-F-", "-G-", "-H-","_1.", "_2.", "_3.", "_4.", "_5.", "_6.", "_7.", "_8.", "_9." };
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (string path in tdDownloadList)
+            {
+                DirectoryInfo TheFolder = new DirectoryInfo(path);
+                if(TheFolder.Exists)
+          
+                    foreach (FileInfo fileInfo in TheFolder.GetFiles("*", SearchOption.AllDirectories))
+                    {
+     
+                        
+
+                            if (fileInfo.Length / 1024 / 1024 > 60)
+                        {
+                            string fileName = Path.GetFileName(fileInfo.Name.ToUpper());
+                            bool ifContinue = false;
+                            foreach (string term in arrayList)
+                            {
+                                if (fileName.Contains(term))
+                                {
+                                    Console.WriteLine(fileName);
+                                    ifContinue = true;
+                                    break;
+                                }
+                            }
+                            if (ifContinue) continue;
+                            string vid = idRegex1.Match(Path.GetFileNameWithoutExtension(fileInfo.Name.ToUpper().Replace("U15XX.",""))).Value.Replace("-", "").Replace("_", "");
+                        string letter = "";
+                        string number = "";
+                        bool isEndofLetter = false;
+                        for (int i = 0; i < vid.Length; i++)                        //修改   对于出现KIDM235A  KIDM235B
+                            if (reg1.IsMatch(vid[i].ToString()))
+                            {
+                                if (isEndofLetter)
+                                    break;
+                                else
+                                    letter += vid[i];
+                            }
+                            else
+                            {
+                                number += vid[i];
+                                isEndofLetter = true;
+                            }
+                        if (letter.ToUpper().EndsWith("VR") || letter.ToUpper().EndsWith("VS"))
+                            continue;
+                        if (dic.ContainsKey(vid))
+                            {
+                                dic[vid].Add(fileInfo);
+                            }
+                            else
+                            {
+                                dic.Add(vid, new ArrayList { fileInfo });
+                            }
+                        }
+                    }
+      
+            }
+            foreach (var item in dic)
+
+            {
+                if ( item.Value.Count > 1)
+                {
+                    string path="";
+                    bool isValid = false;
+                    foreach (FileInfo fileInfo in item.Value)
+                    {
+
+
+
+                        isValid = false;
+                        if (!fileInfo.Extension.EndsWith("td"))
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            path = fileInfo.FullName;
+                        }
+
+                     
+                        
+                    }
+                    if (isValid)
+                    {
+                        MyFileInfo myFileInfo = new MyFileInfo(item.Key, path);
+                        resultList.Add(myFileInfo);
+                    }
+
+                }
+
+            }
+            resultList.Sort(new myReverserClass());
+            dataGridView2.DataSource = resultList;
+            dataGridView2.Refresh();
+        }
+        Regex reg1 = new Regex("[A-Z]");
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = dataGridView2.CurrentCell.Value.ToString();
+            string letter = "";
+            string number = "";
+            bool isEndofLetter = false;
+            for (int i = 0; i < id.Length; i++)                        //修改   对于出现KIDM235A  KIDM235B
+                if (reg1.IsMatch(id[i].ToString()))
+                {
+                    if (isEndofLetter)
+                        break;
+                    else
+                        letter += id[i];
+                }
+                else
+                {
+                    number += id[i];
+                    isEndofLetter = true;
+                }
+
+            string str = "C:\\Progra~1\\Everything\\Everything.exe -search \"" + letter + " " + number + "\"";
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+            p.StandardInput.WriteLine(str + "&exit");
+
+            p.StandardInput.AutoFlush = true;
+
+            Clipboard.SetText(letter + " " + number);
         }
     }
 
@@ -207,8 +470,9 @@ namespace MoveFile
     public class MyFileInfo
     {
 
-        public MyFileInfo(string path)
+        public MyFileInfo(string fileName, string path)
         {
+            this.FileName = fileName;
             this.Path = path;
         }
         string path;
@@ -218,5 +482,24 @@ namespace MoveFile
             get { return path; }
             set { path = value; }
         }
+
+        public string FileName { get => fileName; set => fileName = value; }
+
+        string fileName;
+
     }
+
+    public class myReverserClass : IComparer
+    {
+
+        // Calls CaseInsensitiveComparer.Compare with the parameters reversed.
+        int IComparer.Compare(Object x, Object y)
+        {
+            MyFileInfo myFileInfoX = (MyFileInfo)x;
+            MyFileInfo myFileInfoY = (MyFileInfo)y;
+            return ((new CaseInsensitiveComparer()).Compare(myFileInfoY.Path, myFileInfoX.Path));
+        }
+
+    }
+
 }
